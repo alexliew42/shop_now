b98fb83730a8dfb2e1865070cf18ee8320eced01
Alex Liew <liewtwoo42@gmail.com>
2023-10-05 14:09:47 -0400

    add orders model and controller

--- app/controllers/cartedproducts_controller.rb
+++ app/controllers/cartedproducts_controller.rb
@@ -5,6 +5,15 @@ class CartedproductsController < ApplicationController
       product_id: params[:product_id],
       status: 'carted'
     )
-    
+    @carted_product.save
+  end
+
+  def index
+    if current_user
+      @carted_products = CartedProduct.where(status:'carted', user_id: current_user.id)
+    else
+      @carted_products = {message: 'you must be logged in'}
+    end
+    render json: @carted_products
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :products
   resources :users
   post "/sessions" => "sessions#create"
-  resources :cartedproduct
+  post "/cartedproducts" => "cartedproducts#create"
+  get "/cartedproducts" => "cartedproducts#index"
 end
